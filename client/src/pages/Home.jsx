d8bb8df04a011e129fdda36c2f34b54a8684fddf
angelol-git <ang.ladores.dev@gmail.com>
2025-09-12 14:59:35 -0400

    view recipe css at home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,7 +28,6 @@ function Home() {
     getData();
   }, []);
 
-  console.log(recipes);
   async function handleLogout() {
     try {
       const result = await fetch("http://localhost:8080/api/auth/logout", {
@@ -46,8 +45,20 @@ function Home() {
     }
   }
 
+  function formatDescription(description) {
+    if (description.length > 125) {
+      let truncatedString = description.slice(0, 125).concat("...");
+      return truncatedString;
+    }
+    return description;
+  }
+  function formatDate(dateString) {
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  }
+
   return (
-    <div className="text-text-primary bg-base p-5 lg:p-15 flex flex-col gap-5">
+    <div className="text-text-primary bg-base p-5 lg:p-15 flex flex-col h-screen gap-5">
       <div className="flex gap-5 justify-between items-center">
         <h2>User: {user.email}</h2>
         <button
@@ -74,10 +85,28 @@ function Home() {
           <div>Items({recipes?.length ?? "..."})</div>
           <Link
             to="/add"
-            className="cursor-pointer rounded-lg border-white border-1 px-2 py-1"
+            className="cursor-pointer rounded-lg border-black border-1 px-2 py-1"
           >
             + Add
           </Link>
+        </div>
+        <div className="grid grid-cols-2 gap-4 md:flex">
+          {recipes?.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="border-black/40 border-1 rounded-tr-xl rounded-br-xl rounded-tl-sm rounded-bl-sm p-3 flex flex-col gap-3 max-w-[250px] cursor-pointer"
+              >
+                <h3 className="font-bold font-lora text-xl">{item.title}</h3>
+                <p className="text-text-secondary">
+                  {formatDescription(item.description)}
+                </p>
+                <p className="text-text-secondary/60">
+                  {formatDate(item.created_at)}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
