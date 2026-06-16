5a6ba66fdb05f14b9e4bff95e4273afdd687dc0f
angelol-git <ang.ladores.dev@gmail.com>
2026-06-16 18:13:06 -0400

    chore: restart migration and db file

--- server/migrations/002_add_completed_to_recipe_items.sql
+++
@@ -1,7 +0,0 @@
-PRAGMA foreign_keys = ON;
-
-ALTER TABLE recipe_version_ingredients
-ADD COLUMN completed INTEGER NOT NULL DEFAULT 0 CHECK (completed IN (0, 1));
-
-ALTER TABLE recipe_version_steps
-ADD COLUMN completed INTEGER NOT NULL DEFAULT 0 CHECK (completed IN (0, 1));
