4562da5dad0907a593d4ed92be2674f764f75fde
angelol-git <ang.ladores.dev@gmail.com>
2026-06-19 04:31:16 -0400

    ui(Toast): move Toast position down

--- client/src/components/Toast.tsx
+++ client/src/components/Toast.tsx
@@ -12,7 +12,7 @@ function Toast({ message, onClose, type = "error" }: ToastProps) {
     <div
       role="alert"
       aria-live="assertive"
-      className={` ${type === "error" ? "bg-rose" : "bg-accent"} center fixed top-15 left-1/2 z-[200] flex w-[calc(100%-2rem)] max-w-screen-md -translate-x-1/2 transform justify-between gap-2 rounded-lg p-4 text-sm text-white shadow-lg sm:w-auto`}
+      className={` ${type === "error" ? "bg-rose" : "bg-accent"} center fixed top-40 left-1/2 z-[200] flex w-[calc(100%-2rem)] max-w-screen-md -translate-x-1/2 transform justify-between gap-2 rounded-lg p-4 text-sm text-white shadow-lg sm:w-auto`}
     >
       <span>{message}</span>
       <button className="cursor-pointer underline" onClick={onClose}>
